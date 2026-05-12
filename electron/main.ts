7d209f1d7880f89b717bb3cdfe081d0708cd8329
gvinothkumar87 <gvinothkumar87@gmail.com>
2026-05-12 16:36:43 +0530

    dsfasdfasdf

--- electron/main.ts
+++ electron/main.ts
@@ -29,6 +29,7 @@ let win: BrowserWindow | null
 
 function createWindow() {
   win = new BrowserWindow({
+    title: 'grm sales',
     icon: path.join(process.env.VITE_PUBLIC || '', 'electron-vite.svg'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.mjs'),
